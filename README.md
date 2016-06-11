53a57163175e9b347f79b9d6286ea39436b16b35
Anh <quanganh.tran94@gmail.com>
2016-06-11 09:28:38 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fab-recycling-server
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/MaterialController.js
@@ -0,0 +1,11 @@
+/**
+ * MaterialController
+ *
+ * @description :: Server-side logic for managing materials
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
